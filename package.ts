467b64268fcbf14176072e858e1f318785b0c031
Anubhav Saini <iamanubhavsaini@gmail.com>
2021-03-23 13:31:12 +0530

    Create package.ts

---
+++ package.ts
@@ -0,0 +1,52 @@
+enum TypesEnum {
+    INFO = 'INFO',
+    ERROR = 'ERROR',
+    WARN = 'WARN',
+    LOG = 'LOG'
+}
+
+const TYPES = {
+    INFO: console.info,
+    ERROR: console.error,
+    WARN: console.warn,
+    LOG: console.log
+};
+
+function exitOnError(severity: TypesEnum) {
+    const exitMessage = 'Exiting application on ERROR';
+    if (severity === TypesEnum.ERROR) {
+        console.log(exitMessage);
+        console.error(exitMessage);
+        process.exit(1);
+    }
+}
+
+function log(tag: string, message = '', severity: TypesEnum) {
+    let _logFn: ((tag?: string, message?: string) => never | any);
+    switch (severity) {
+        case TypesEnum.ERROR:
+            _logFn = TYPES.ERROR;
+            break;
+        case TypesEnum.INFO:
+            _logFn = TYPES.INFO;
+            break;
+        case TypesEnum.WARN:
+            _logFn = TYPES.WARN;
+            break;
+        case TypesEnum.LOG:
+            _logFn = TYPES.LOG;
+            break;
+        default:
+            _logFn = TYPES.INFO;
+    }
+
+    _logFn(tag, message);
+
+    exitOnError(severity);
+}
+
+export default log;
+
+export {
+    TypesEnum
+};
